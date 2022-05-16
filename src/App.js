3d923dcaa8b3e75b1ae2f7d93a0a0114322ccea4
Kiruthikaa05 <105360432+Kiruthikaa05@users.noreply.github.com>
2022-05-16 20:37:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Greet from './Component/PropsFunc';
 import Welcome from './Component/propsclass';
 import { Message } from './Component/stateclass';
+import Form from './Forms';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Greet name='john smith' funname='smith' />
       <Welcome name='diya' />
       <Message />
+      <Form/>
     </div>
   );
 }
